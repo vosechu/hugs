3e2c8b49aabc891f5005bc9e209fee6ecb0a0a78
dylantack <dylan@Jasper.local>
2009-12-15 15:55:38 -0800

    coder compliance

--- element.tpl.php
+++ element.tpl.php
@@ -1,9 +1,7 @@
-<?php if (!empty($elements['elements']) && !empty($elements['elements'][0])) { ?>
+<?php if (!empty($elements['elements']) && !empty($elements['elements'][0])): ?>
 <div id="<?php print $elements['title'] ?>-wrapper" class="element">
-  <?php 
-  foreach ($elements['elements'] as $element) {
-    print $element;
-  }
-  ?>
+  <?php foreach ($elements['elements'] as $element): ?>
+    <?php print $element; ?>
+  <?php endforeach; ?>
 </div>
-<?php } ?>
+<?php endif;
