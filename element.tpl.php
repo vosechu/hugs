96e7148eade8278b07bea0d4822539ed4d9a2cb6
Chuck Lauer Vose <vosechu@gmail.com>
2013-08-31 19:57:11 -0700

    remove drupal stuff, replace with image of kitten

--- element.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php if (!empty($elements['elements']) && !empty($elements['elements'][0])): ?>
-<div id="<?php print $elements['title'] ?>-wrapper" class="element">
-  <?php foreach ($elements['elements'] as $element): ?>
-    <?php print $element; ?>
-  <?php endforeach; ?>
-</div>
-<?php endif;
